3ff880e8ccc0e1c744900cdd96e5720e603324d7
Logan Dhillon <dev@logandhillon.com>
2025-12-04 00:51:07 -0500

    feat: win/lose bg gradient

--- src/main/java/com/logandhillon/typeofwar/game/TypeOfWarScene.java
+++ src/main/java/com/logandhillon/typeofwar/game/TypeOfWarScene.java
@@ -7,6 +7,9 @@ import com.logandhillon.typeofwar.entity.SentenceEntity;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 
 /**
  * The main game loop for Type of War.
@@ -14,8 +17,20 @@ import javafx.scene.paint.Color;
  * @author Logan Dhillon
  */
 public class TypeOfWarScene extends GameScene {
+    private static final LinearGradient BG_WINNING = new LinearGradient(
+            0, 1, 0, 0, true, CycleMethod.NO_CYCLE,
+            new Stop(0, Color.rgb(8, 4, 44)),
+            new Stop(0.33, Color.BLACK));
+
+    private static final LinearGradient BG_LOSING = new LinearGradient(
+            0, 1, 0, 0, true, CycleMethod.NO_CYCLE,
+            new Stop(0, Color.rgb(39, 3, 3)),
+            new Stop(0.33, Color.BLACK));
+
     private final SentenceEntity sentence;
     private final RopeEntity     rope;
+
+    private boolean isWinning = true;
 
     public TypeOfWarScene() {
         sentence = new SentenceEntity(WINDOW_WIDTH / 2f, (WINDOW_HEIGHT + 300) / 2f);
@@ -34,7 +49,7 @@ public class TypeOfWarScene extends GameScene {
     @Override
     protected void render(GraphicsContext g) {
         // background
-        g.setFill(Color.BLACK);
+        g.setFill(isWinning ? BG_WINNING : BG_LOSING);
         g.fillRect(0, 0, 1280, 720);
 
         // render all other entities
