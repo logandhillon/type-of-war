d4b93690a98a7a26d65e2ac99c465f4779a0d997
Logan Dhillon <dev@logandhillon.com>
2025-12-07 02:50:14 -0500

    feat: change menu bg color

--- src/main/java/com/logandhillon/typeofwar/game/MainMenuScene.java
+++ src/main/java/com/logandhillon/typeofwar/game/MainMenuScene.java
@@ -2,6 +2,7 @@ package com.logandhillon.typeofwar.game;
 
 import com.logandhillon.typeofwar.engine.UIScene;
 import com.logandhillon.typeofwar.entity.ui.MenuButton;
+import com.logandhillon.typeofwar.resource.Colors;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -42,7 +43,7 @@ public class MainMenuScene extends UIScene {
     @Override
     protected void render(GraphicsContext g) {
         // background
-        g.setFill(Color.BLACK);
+        g.setFill(Colors.BG_WINNING);
         g.fillRect(0, 0, WINDOW_WIDTH.doubleValue(), WINDOW_HEIGHT.doubleValue());
 
         // render all other entities
