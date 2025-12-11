198c473a9f405987b7405116db142b30d7b6e4d7
RossJck <1rossjac5@hdsb.ca>
2025-12-11 02:03:10 -0500

    docs: javadocs + comments for LobbyGameScene and LobbyPlayerEntity

--- src/main/java/com/logandhillon/typeofwar/entity/ui/LobbyPlayerEntity.java
+++ src/main/java/com/logandhillon/typeofwar/entity/ui/LobbyPlayerEntity.java
@@ -9,6 +9,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
+/**
+ * An entity used in {@link com.logandhillon.typeofwar.game.LobbyGameScene} to show a graphical representation of a
+ * user in the lobby's name, colour, and latency
+ *
+ * @author Jack Ross
+ */
 public class LobbyPlayerEntity extends BoundEntity<UIScene> {
     private static final Font LABEL_FONT = Font.font(Fonts.DM_MONO_MEDIUM, 16);
     private final Color color;
@@ -16,6 +22,12 @@ public class LobbyPlayerEntity extends BoundEntity<UIScene> {
     private final int ping;
     private static final int WIDTH = 225;
 
+    /**
+     *
+     * @param color the color of the given player's skin
+     * @param playerName the given player's chosen name
+     * @param ping the given player's latency
+     */
     public LobbyPlayerEntity(Color color, String playerName, int ping){
         super(0, 0);
         this.color = color;
@@ -25,17 +37,22 @@ public class LobbyPlayerEntity extends BoundEntity<UIScene> {
     @Override
     protected void onRender(GraphicsContext g, float x, float y) {
         g.setFont(LABEL_FONT);
-
         g.setFill(color);
+
+        // render player skin
         g.fillRect(x, y, 32, 32);
 
         g.setTextAlign(TextAlignment.LEFT);
         g.setTextBaseline(VPos.CENTER);
         g.setFill(Color.WHITE);
+
+        // render player name
         g.fillText(this.playerName, x + 48, y + 16);
 
         g.setTextAlign(TextAlignment.RIGHT);
         g.setFill(Color.GREY);
+
+        // render player latency
         g.fillText(this.ping + "ms", x + WIDTH, y + 16);
     }
 
--- src/main/java/com/logandhillon/typeofwar/game/LobbyGameScene.java
+++ src/main/java/com/logandhillon/typeofwar/game/LobbyGameScene.java
@@ -17,24 +17,43 @@ import javafx.scene.text.TextAlignment;
 import static com.logandhillon.typeofwar.TypeOfWar.WINDOW_HEIGHT;
 import static com.logandhillon.typeofwar.TypeOfWar.WINDOW_WIDTH;
 
+/**
+ * The lobby game menu shows all users in a lobby and allots the host with special permissions to start the
+ * {@link TypeOfWarScene}
+ *
+ * @author Jack Ross
+ * @see LobbyPlayerEntity
+ */
 public class LobbyGameScene extends UIScene {
 
 
     private static final Font LABEL_FONT = Font.font(Fonts.DM_MONO_MEDIUM, 18);
     private static final float ENTITY_GAP = 48;
 
+    /**
+     *
+     * @param mgr the {@link GameSceneManager} responsible for switching active scenes.
+     * @param roomName the name of the lobby stated in {@link HostGameScene}
+     * @param isHosting determines if the user is the host of the given lobby or not
+     * @param team1 the list of the players on team 1
+     * @param team2 the list of the players on team 2
+     */
     public LobbyGameScene(GameSceneManager mgr, String roomName, boolean isHosting, LobbyPlayerEntity[] team1, LobbyPlayerEntity[] team2){
 
+        // containers for each team
         PlayerContainer leftContainer = new PlayerContainer(16, 47, 257, 206);
         PlayerContainer rightContainer = new PlayerContainer(289, 47, 257, 206);
+
+        // labels for each container
         ContainerLabel leftLabel = new ContainerLabel(16, 16, 1);
         ContainerLabel rightLabel = new ContainerLabel(289, 16, 2);
 
 
-
+        // shows different buttons at bottom depending on if the user is hosting
         DarkMenuButton startButton = new DarkMenuButton(isHosting? "START GAME" : "WAITING FOR HOST TO START...", 16, 269, 530, 48, ()-> {
                 //TODO #6: Start the game for everyone
             });
+
         if(!isHosting){
             startButton.setActive(false, true);
         }
@@ -43,6 +62,8 @@ public class LobbyGameScene extends UIScene {
 
         addEntity(lobbyModal);
 
+        // iterate through each team's list of players
+
         float dy = 0;
 
         for(LobbyPlayerEntity p : team1){
@@ -50,7 +71,6 @@ public class LobbyGameScene extends UIScene {
             dy += ENTITY_GAP;
             lobbyModal.addEntity(p);
         }
-
         dy = 0;
         for(LobbyPlayerEntity p : team2){
             p.setPosition(305, p.getY() + dy + 128);
