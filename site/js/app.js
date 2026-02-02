748bd5f687bb3da052eca1603a36cddf0d28521c
Logan Dhillon <dev@logandhillon.com>
2026-02-02 00:14:36 -0500

    feat(www): load page js

---
+++ site/js/app.js
@@ -0,0 +1,5 @@
+function onLoad() {
+  document.getElementById("copyright").textContent = `© 2025-${new Date().getFullYear()} logandhillon.com. All rights reserved.`;
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
